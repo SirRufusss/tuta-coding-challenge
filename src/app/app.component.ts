a977263129566cca24a1aa15d483f5dcc676c1d9
SirRufusss <sterz_p@proton.me>
2024-05-26 18:27:36 +0200

    adding Components header+url-checker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UrlCheckerComponent } from "./url-checker/url-checker.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, UrlCheckerComponent]
 })
 export class AppComponent {
   title = 'tuta-coding-challenge';
