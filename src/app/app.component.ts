c269c6f845594dd6e776738ed9d739dbf09bb572
SirRufusss <sterz_p@proton.me>
2024-05-26 19:14:53 +0200

    Replace all content with local version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UrlCheckerComponent } from "./url-checker/url-checker.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, UrlCheckerComponent]
+
 })
 export class AppComponent {
   title = 'tuta-coding-challenge';
